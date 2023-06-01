bfb84dbad9a3fe3cc2255c001774f04b94c007d7
drowsy-probius <k123s456h@gmail.com>
2023-06-01 17:57:13 +0900

    debug

--- logic_twitch.py
+++ logic_twitch.py
@@ -469,7 +469,9 @@ class LogicTwitch(LogicModuleBase):
       self.set_streamlink_session()
     # 5.0.0 이상에서는 (plugin_name, streamlink_plugin_class, url) 으로 할당해야 함.
     (plugin_name, streamlink_plugin_class, url) = self.streamlink_session.resolve_url(f'https://www.twitch.tv/{streamer_id}')
-    print(plugin_name, dir(streamlink_plugin_class), url)
+    logger.debug(plugin_name)
+    logger.debug(dir(streamlink_plugin_class))
+    logger.debug(url)
     streamlink_plugin = streamlink_plugin_class(self.streamlink_session, url)
     streams = streamlink_plugin.streams()
     return {q:streams[q] for q in streams}
